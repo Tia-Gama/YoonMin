cdca381a67539f31c9fae82f5f0b7919f9b57359
Tia-Gama <90091552+Tia-Gama@users.noreply.github.com>
2021-12-24 10:56:52 +0700

    Revert "Final"

--- Laba4_2/Program.cs
+++ Laba4_2/Program.cs
@@ -10,12 +10,6 @@ namespace Laba4_2
     {
         static void Main(string[] args)
         {
-            int a = Int32.Parse(Console.ReadLine());
-            int b = Int32.Parse(Console.ReadLine());
-
-            int c = a + b; 
-
-            int r = a - b;
         }
     }
 }
